529b92090092fe8cc1b4223070cb181b26038f2f
jeanbruder <jeanbruder@users.noreply.github.com>
2015-01-29 16:33:38 +0100

    Prise en compte DB SQLite

--- QueLLS/lib/config.php
+++ QueLLS/lib/config.php
@@ -1,4 +1,10 @@
 <?php
+  // type de DB (MySQL, SQLite)
+  $db_type='sqlite';
+  
+  // variables de connexion SQLite
+  $db_file='smart_plug.db';
+  
 	// variables de connexion MySql
 	$db_hostname='localhost';
 	$db_login='sp_admin';
